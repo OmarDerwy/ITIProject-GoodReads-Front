3365b2aa6833eedc5e6065bde4f2f1129a9b6844
fazm1 <farid.azzam.2001@gmail.com>
2025-01-31 23:09:29 +0200

    navbar 3

--- src/components/general/navbar.jsx
+++ src/components/general/navbar.jsx
@@ -1,158 +1,69 @@
-import {
-    IconBook,
-    IconChartPie3,
-    IconChevronDown,
-    IconCode,
-    IconCoin,
-    IconFingerprint,
-    IconNotification,
-  } from '@tabler/icons-react';
-  import {
-    Anchor,
-    Box,
-    Burger,
-    Button,
-    Center,
-    Collapse,
-    Divider,
-    Drawer,
-    Group,
-    HoverCard,
-    ScrollArea,
-    SimpleGrid,
-    Text,
-    ThemeIcon,
-    UnstyledButton,
-    useMantineTheme,
-  } from '@mantine/core';
-  import { useDisclosure } from '@mantine/hooks';
-  import { MantineLogo } from '@mantinex/mantine-logo';
-  import classes from './navbar.module.css';
-  
-  const mockdata = [
-    {
-      icon: IconCode,
-      title: 'Open source',
-      description: 'This Pokémon’s cry is very loud and distracting',
-    },
-    {
-      icon: IconCoin,
-      title: 'Free for everyone',
-      description: 'The fluid of Smeargle’s tail secretions changes',
-    },
-    {
-      icon: IconBook,
-      title: 'Documentation',
-      description: 'Yanma is capable of seeing 360 degrees without',
-    },
-    {
-      icon: IconFingerprint,
-      title: 'Security',
-      description: 'The shell’s rounded shape and the grooves on its.',
-    },
-    {
-      icon: IconChartPie3,
-      title: 'Analytics',
-      description: 'This Pokémon uses its flying ability to quickly chase',
-    },
-    {
-      icon: IconNotification,
-      title: 'Notifications',
-      description: 'Combusken battles with the intensely hot flames it spews',
-    },
-  ];
-  
+import { IconSearch } from '@tabler/icons-react';
+import { Autocomplete, Burger, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './navbar.module.css';
 
+const links = [
+  { link: '/about', label: 'Home' },
+  { link: '/pricing', label: 'Categories' },
+  { link: '/learn', label: 'Books' },
+  { link: '/community', label: 'Authors' },
 
-  function Navbar() {
-    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-    const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const theme = useMantineTheme();
-  
-    const links = mockdata.map((item) => (
-      <UnstyledButton className={classes.subLink} key={item.title}>
-        <Group wrap="nowrap" align="flex-start">
-          <ThemeIcon size={34} variant="default" radius="md">
-            <item.icon size={22} color={theme.colors.blue[6]} />
-          </ThemeIcon>
-          <div>
-            <Text size="sm" fw={500}>
-              {item.title}
-            </Text>
-            <Text size="xs" c="dimmed">
-              {item.description}
-            </Text>
-          </div>
+];
+const userLinks = [
+  { link: '/community', label: 'Login' },
+  { link: '/community', label: 'Sign Up' },
+]
+
+function Navbar() {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      onClick={(event) => event.preventDefault()}
+    >
+      {link.label}
+    </a>
+  ));
+  const userItems = userLinks.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      onClick={(event) => event.preventDefault()}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.inner}>
+        <Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          <a>Goodreads</a>
         </Group>
-      </UnstyledButton>
-    ));
-  
-    return (
-      <Box pb={120} >
-        <header className={classes.header}>
-          <Group justify="space-between" h="100%">
-            <MantineLogo size={30} />
-  
-            <Group h="100%" gap={0} visibleFrom="sm">
-              <a href="#" className={classes.link}>
-                Home
-              </a>
-              <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-               
-  
-               
-              </HoverCard>
-              <a href="#" className={classes.link}>
-                Learn
-              </a>
-              <a href="#" className={classes.link}>
-                Academy
-              </a>
-            </Group>
-  
-            <Group visibleFrom="sm">
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
-  
-            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+
+        <Group>
+          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+            {items}
           </Group>
-        </header>
-  
-        <Drawer
-          opened={drawerOpened}
-          onClose={closeDrawer}
-          size="100%"
-          padding="md"
-          title="Navigation"
-          hiddenFrom="sm"
-          zIndex={1000000}
-        >
-          <ScrollArea h="calc(100vh - 80px" mx="-md">
-            <Divider my="sm" />
-  
-            <a href="#" className={classes.link}>
-              Home
-            </a>
-            
-          
-            <a href="#" className={classes.link}>
-              Learn
-            </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
-  
-            <Divider my="sm" />
-  
-            <Group justify="center" grow pb="xl" px="md">
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
-          </ScrollArea>
-        </Drawer>
-      </Box>
-    );
-  }
-  
-  export default Navbar;
+          <Autocomplete
+            className={classes.search}
+            placeholder="Search"
+            leftSection={<IconSearch size={16} stroke={1.5} />}
+            data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
+            visibleFrom="xs"
+          />
+        {userItems}
+        </Group>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
