4bcc2303e6c1bf5310b9f08250b50ca7f2871f36
OmarDerwy <OmarDerwy@gmail.com>
2025-01-31 22:29:29 +0200

    MP-4 add nav

--- src/components/landingpage/HeroSection.jsx
+++ src/components/landingpage/HeroSection.jsx
@@ -1,42 +1,72 @@
-import { Button, Container, Group, Text } from '@mantine/core';
+import { Button, Container, Group, Text, Grid } from '@mantine/core';
 // import { GithubIcon } from '@mantinex/dev-icons';
 import classes from './../../styles/landingpage/HeroSection.module.css';
+import EtherealBook from '/EtherealBook.png'
+
+//react
+import { useState, useEffect } from 'react';
 
 export default function HeroSection() {
+  function ImageWithHueShift({ src, alt, className }) {
+    const [hue, setHue] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setHue((hue + 1) % 360);
+      }, 10);
+  
+      return () => clearInterval(interval);
+    }, [hue]);
+  
+    return (
+      <img
+        src={src}
+        alt={alt}
+        className={className}
+        style={{
+          filter: `hue-rotate(${hue}deg)`,
+        }}
+      />
+    );
+  }
   return (
     <div className={classes.wrapper}>
-      <Container size={700} className={classes.inner}>
-        <h1 className={classes.title}>
-          <Text component="span" variant="gradient" gradient={{ from: 'green', to: 'lightgreen' }} inherit>
-            Discover
-          </Text>{' '}
-            a vast number of books to read
-        </h1>
-
-        <Text className={classes.description} color="dimmed">
-          create your own libaray of books and discover more that fit your tastes from our database!
-        </Text>
-
-        <Group className={classes.controls}>
-          <Button
-            size="xl"
-            className={classes.control}
-            variant="gradient"
-            gradient={{ from: 'green', to: 'lightgreen' }}
-          >
-            Get started
-          </Button>
-
-          <Button
-            component="a"
-            href="#"
-            size="xl"
-            variant="default"
-            className={classes.control}
-          >
-            Login
-          </Button>
-        </Group>
+      <Container size={900} className={classes.inner}>
+        <Grid>
+          <Grid.Col span={{base: 12, md: 6, lg: 6, xl: 6}} order={{base: 1, md: 2, lg: 2}}>
+            <ImageWithHueShift src={EtherealBook} alt="hero" className={classes.image} />
+          </Grid.Col>
+          <Grid.Col span={{base: 12, md: 6, lg: 6, xl: 6}} order={{base: 2, md: 1, lg: 1}}>
+            <h1 className={classes.title}>
+              <Text component="span" variant="gradient" gradient={{ from: 'green', to: 'lightgreen' }} inherit>
+                Discover
+              </Text>{' '}
+                a vast number of books to read
+            </h1>
+            <Text className={classes.description} color="dimmed">
+              create your own libaray of books and discover more that fit your tastes from our database!
+            </Text>
+            <Group className={classes.controls}>
+              <Button
+                size="xl"
+                className={classes.control}
+                variant="gradient"
+                gradient={{ from: 'green', to: 'lightgreen' }}
+              >
+                Get started
+              </Button>
+              <Button
+                component="a"
+                href="#"
+                size="xl"
+                variant="default"
+                className={classes.control}
+              >
+                Login
+              </Button>
+            </Group>
+          </Grid.Col>
+        </Grid>
       </Container>
     </div>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+
+//components
 import HeroSection from '../components/landingpage/HeroSection'
+import MinimalNavbar from '../components/landingpage/MinimalNavBar'
 
 export default function LandingPage() {
   return (
     <>
+      <MinimalNavbar/>
       <HeroSection/>
     </>
   )
