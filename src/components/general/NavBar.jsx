ca6451350cf37ee17e243549f4d1007f6375f37e
fazm1 <farid.azzam.2001@gmail.com>
2025-02-02 15:13:41 +0200

    added navbar navigation and login/signup buttons

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -1,69 +1,74 @@
 // import { IconSearch } from "@tabler/icons-react"
-import { Autocomplete, Burger, Group } from "@mantine/core";
+import { Autocomplete, Burger, Button, Group } from "@mantine/core";
+import { useNavigate } from 'react-router-dom';
 import { useDisclosure } from "@mantine/hooks";
 import classes from "../../styles/general/Navbar.module.css";
 import BooksLogo from "/Logomark.svg";
 import { CiSearch } from "react-icons/ci";
-
+import { Link } from "react-router-dom";
 
 const links = [
-  { link: "/about", label: "Home" },
-  { link: "/pricing", label: "Categories" },
-  { link: "/learn", label: "Books" },
-  { link: "/community", label: "Authors" },
-];
-const userLinks = [
-  { link: "/community", label: "Login" },
-  { link: "/community", label: "Sign Up" },
+  { link: "/", label: "Home" },
+  { link: "/Categories", label: "Categories" },
+  { link: "/Books", label: "Books" },
+  { link: "/Authors", label: "Authors" },
 ];
 
 function Navbar() {
+  const navigate = useNavigate();
   const [opened, { toggle }] = useDisclosure(false);
 
   const items = links.map((link) => (
-    <a
+    <Link
       key={link.label}
-      href={link.link}
+      to={link.link}
       className={classes.link}
-      onClick={(event) => event.preventDefault()}
     >
       {link.label}
-    </a>
-  ));
-  const userItems = userLinks.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      onClick={(event) => event.preventDefault()}
-    >
-      {link.label}
-    </a>
+    </Link>
   ));
 
   return (
     <header className={classes.header}>
       <div className={classes.inner}>
-        <Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-          <a href="#" className={classes.inner}>
+        <Group wrap="nowrap" align="flex-start" className="Logo">
+          <Link style={{textDecoration: "none"}} to="#" className={classes.inner}>
             <img src={BooksLogo} alt="Books Logo" />
-            <p className={classes.p}> Books</p>
-          </a>
+            <p className={classes.p}>Books</p>
+          </Link>
         </Group>
 
         <Group>
-          <Group ml={420} gap={5} className={classes.links} visibleFrom="sm">
+          <Group gap={5} className={classes.links} visibleFrom="sm">
             {items}
           </Group>
           <Autocomplete
             className={classes.search}
             placeholder="Search"
             leftSection={<CiSearch />}
-            
             visibleFrom="xs"
           />
-          {userItems}
+
+          <Button
+            size="sm"
+            onClick={() => navigate("/Login")}
+            className={classes.control}
+            variant="gradient"
+            gradient={{ from: "green", to: "lightgreen" }}
+          >
+            Login
+          </Button>
+          <Button
+            component="a"
+            onClick={() => navigate("/sign-up")}
+            size="sm"
+            variant="default"
+            className={classes.control}
+          >
+            Sign Up
+          </Button>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+
         </Group>
       </div>
     </header>
