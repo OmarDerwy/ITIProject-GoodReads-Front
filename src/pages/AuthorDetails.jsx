01c966c0fb843bfe0055a18d03b5e1b8abc7bf80
fazm1 <farid.azzam.2001@gmail.com>
2025-02-07 14:57:07 +0200

    author details crud finished

--- src/pages/AuthorDetails.jsx
+++ src/pages/AuthorDetails.jsx
@@ -7,7 +7,7 @@ import {
   Select,
   Rating,
   Title,
-  Image
+  Image,
 } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -15,6 +15,7 @@ import axiosInstance from "../apis/config";
 
 export default function AuthorDetails() {
   const [author, setAuthor] = useState({});
+  const [books, setBooks] = useState([]);
   const navigate = useNavigate();
   const authorId = location.pathname.split("/")[2];
 
@@ -23,26 +24,45 @@ export default function AuthorDetails() {
       try {
         const response = await axiosInstance.get(`/api/authors/${authorId}`);
         setAuthor(response.data);
+        setBooks(response.data.books);
       } catch (err) {
         console.log("Error: " + err.message);
       }
     };
     fetchAuthor();
-  }, []);
+  }, [authorId]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const booksData = await Promise.all(
+          books.map(async (book) => {
+            const response = await axiosInstance.get(
+              `/api/books/${book.bookId}`
+            );
+            return response.data;
+          })
+        );
+        setBooks(booksData);
+      } catch (err) {
+        console.log("Error: " + err.message);
+      }
+    };
+    if (author.books && author.books.length > 0) {
+      fetchBooks();
+    }
+  }, [author]);
   return (
     <>
+      {console.log(books)}
       <Paper radius="md" p="lg" bg="var(--mantine-color-body)" m={10}>
         <Grid>
           <Grid.Col span="auto">
             {" "}
-            <Avatar
-              src={author.imageUrl}
-              size={210}
-              radius={210}
-            />
+            <Avatar src={author.imageUrl} size={210} radius={210} />
           </Grid.Col>
           <Grid.Col span={10}>
-            <Title ta="left"  mb={5} order={2}>
+            <Title ta="left" mb={5} order={2}>
               {author.authorName}
             </Title>
             <Text ta="left" fz="md" fw={500} mb={20}>
@@ -59,44 +79,56 @@ export default function AuthorDetails() {
         Books released:
       </Title>
 
-      {/*map here*/}
-      <Paper
-        radius="md"
-        withBorder
-        p="lg"
-        bg="var(--mantine-color-body)"
-        m={50}
-      >
-        <Grid>
-          <Grid.Col span="auto">
-
-            
-            <Image
-              radius="md"
-              h="200"
-              w="180"
-              fit="fill"
-              src="dagdadgas"
-            />
-          </Grid.Col>
-          <Grid.Col span={8}>
-            <Text ta="left" fz="lg" fw={500} mb={5}>
-              Book Name
-            </Text>
-            <Text ta="left" fz="md" fw={500} mb={20} span>
-              <Rating value={2.5} fractions={2} readOnly /> 2.5 stars - 2 ratings
-            </Text>
-          </Grid.Col>
-          <Grid.Col span="auto">
-            <Select
-              placeholder="Read status"
-              data={["Currently read", "Want to read", "Read"]}
-              defaultValue=""
-            />
-            <Rating defaultValue={0} fractions={2} mt={20} />
-          </Grid.Col>
-        </Grid>
-      </Paper>
+      {books.map((book, index) => (
+        <Paper
+          key={index}
+          radius="md"
+          withBorder
+          p="lg"
+          bg="var(--mantine-color-body)"
+          m={50}
+        >
+          <Grid>
+            <Grid.Col span="auto">
+              <Image
+                radius="md"
+                h="200"
+                w="180"
+                fit="fill"
+                src={book.coverImage}
+                onClick={() => navigate(`/books/${book._id}`)}
+                style={{ cursor: "pointer" }}
+              />
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <Text
+                ta="left"
+                fz="lg"
+                fw={700}
+                mb={5}
+                onClick={() => navigate(`/books/${book._id}`)}
+                style={{ cursor: "pointer" }}
+              >
+                {book.bookName}
+              </Text>
+              <Text ta="left" fz="md" fw={500} mb={20} span>
+                <Rating value={book.averageRating} fractions={4} readOnly />{" "}
+                {book.averageRating}
+                {book.averageRating == 1 ? " star" : " stars"} - {book.ratings}
+                {book.ratings == 1 ? " rating" : " ratings"}
+              </Text>
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <Select
+                placeholder="Read status"
+                data={["Currently read", "Want to read", "Read"]}
+                defaultValue=""
+              />
+              <Rating defaultValue={0} fractions={4} mt={20} />
+            </Grid.Col>
+          </Grid>
+        </Paper>
+      ))}
     </>
   );
 }
